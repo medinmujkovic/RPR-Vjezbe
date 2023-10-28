e94204bc8b83c0b302b41c1b8f1d0e4917a44922
Medin <40238764+medinmujkovic@users.noreply.github.com>
2023-10-28 16:43:42 +0200

    add main

---
+++ Main.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ulaz=new Scanner(System.in);
+        int a=ulaz.nextInt();
+        int b=ulaz.nextInt();
+        System.out.print(a+b);
+    }
+}
