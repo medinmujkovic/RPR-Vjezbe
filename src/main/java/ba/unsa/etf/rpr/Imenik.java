983ccf4b9f91af8cb697ebf868baecc429be3ae6
medinmujkovic <medin.mujkovic@hotmail.com>
2023-10-28 20:33:46 +0200

    Postavka

---
+++ src/main/java/ba/unsa/etf/rpr/Imenik.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Set;
+
+public class Imenik {
+    private String ime;
+    private telefonskiBroj broj;
+    public void dodaj(String ime,telefonskiBroj broj)
+    {
+
+    }
+    String dajBroj(String ime){
+        return telefonskiBroj.ispisi();
+    }
+    String dajIme(telefonskiBroj broj)
+    {
+        return null;
+    }
+    String naSlovo(char s){
+        return null;
+    }
+    Set<String>izGrada(FiksniBroj.Grad g){
+        return null;
+    }
+    Set<telefonskiBroj>izGradaBrojevi(FiksniBroj.Grad g)
+    {
+        return null;
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/Program.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+class Program {
+    public static void main(String[] args)
+    {
+        Scanner ulaz=new Scanner(System.in);
+
+    }
+}
